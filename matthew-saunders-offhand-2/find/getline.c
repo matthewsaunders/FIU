c9eef5711e8b12f8f179d7eadd82ebac724639c5
M. Saunders <msaun008@fiu.edu>
2014-09-21 23:48:22 -0400

    worked on find. refactored into find and polish directories

---
+++ matthew-saunders-offhand-2/find/getline.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define MAXLEN 1000 /* max length of any input line */
+
+/* getline: read a line into s, return length */
+int getline(char s[],int lim)
+{
+  int c, i;
+  for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+    s[i] = c;
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
+}
+
