a858758dbed349e5980ed0e758121c7a89eafbe0
M. Saunders <msaun008@fiu.edu>
2014-09-22 23:59:32 -0400

    Finished initial find.c and adding getopt?

--- matthew-saunders-offhand-2/find/find.c
+++ matthew-saunders-offhand-2/find/find.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #define MAXLINE 1000
+
+/* getline: get line into s, return length */
+int getline2(char *line, int max);
 
 /* find: print lines that match pattern from 1st arg */
 main(int argc, char *argv[])
@@ -9,34 +13,48 @@ main(int argc, char *argv[])
   long lineno = 0;
   int c, except = 0, number = 0, found = 0;
 
-  while (--argc > 0 && (*++argv)[0] == '-')
-    while (c = *++argv[0])
-      switch (c) {
-        case 'x':
-          except = 1;
-          break;
-        case 'n':
-          number = 1;
-          break;
-        default:
-          printf("find: illegal option %c\n", c);
-          argc = 0;
-          found = -1;
-          break;
+  while ((c = getopt(argc, argv, "xn")) != -1)
+    switch (c) {
+      case 'x':
+        except = 1;
+        break;
+      case 'n':
+        number = 1;
+        break;
+      default:
+        printf("find: illegal option %c\n", c);
+        argc = 0;
+        found = -1;
+        break;
       }
+  argc -= optind;
+  argv += optind;
 
-  if (argc != 1)
-    printf("Usage: find -x -n pattern\n");
-  else
-    while (getline(line, MAXLINE) > 0) {
-      lineno++;
-      if ((strstr(line, *argv) != NULL) != except) {
-        if (number)
-          printf("%ld:", lineno);
-        printf("%s", line);
-        found++;
-      }
+if (argc != 1)
+  printf("Usage: find -x -n pattern\n");
+else
+  while (getline2(line, MAXLINE) > 0) {
+    lineno++;
+    if ((strstr(line, *argv) != NULL) != except) {
+      if (number)
+        printf("%ld:", lineno);
+      printf("%s", line);
+      found++;
     }
+  }
   return found;
 }
 
+/* getline: get line into s, return length */
+int getline2(char s[], int lim)
+{
+  int c, i;
+  
+  i = 0;
+  while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
+    s[i++] = c;
+  if (c == '\n')
+    s[i++] = c;
+    s[i] = '\0';
+  return i;
+}
