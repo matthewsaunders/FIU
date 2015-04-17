38e9557263d030f9f18d183e460b3cc10f0db726
msaun008 <msaun008@fiu.edu>
2015-04-17 18:33:49 -0400

    refactor header and footer

---
+++ app/recipe/index.php
@@ -0,0 +1,124 @@
+<?php
+	include '../common/header.php';
+?>
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+					<div class="row">
+						<form role="form">
+                            <div class="row">
+    							<div class="col-lg-12">
+    								<h2 class="text-center">Peanut Butter Recipe</h2> 
+    							</div>
+                            </div>
+							<div class="row">
+                                <div class="col-lg-4">
+                                    <img src="img/PeanutButter.jpg" class="img-responsive" alt="peanutButter" width="100%" height="100%">
+                                </div>
+                                <div class="col-lg-8">
+
+                                    <div class="row">
+                                        <div class="col-sm-3"  >
+                                            
+                                        </div>
+                                        <div class="col-sm-9 vcenter"  >
+                                            <a href="">edit recipe</a> - 
+                                            <a href="">add to cookbook</a>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-sm-3"  >
+                                            <h4><strong>Name</strong></h4>
+                                        </div>
+                                        <div class="col-sm-9 vcenter"  >
+                                            <p>Peanut Butter Recipe</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-sm-3"  >
+                                            <h4><strong>Author</strong></h4>
+                                        </div>
+                                        <div class="col-sm-9 vcenter"  >
+                                            <p>John Doe</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-sm-3"  >
+                                            <h4><strong>Difficulty</strong></h4>
+                                        </div>
+                                        <div class="col-sm-9"  >
+                                            <div class="btn-group" role="group" aria-label="...">
+                                              <button type="button" class="btn btn-default active">1</button>
+                                              <button type="button" class="btn btn-default">2</button>
+                                              <button type="button" class="btn btn-default">3</button>
+                                              <button type="button" class="btn btn-default">4</button>
+                                              <button type="button" class="btn btn-default">5</button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-sm-3"  >
+                                            <h4><strong>Cooktime</strong></h4>
+                                        </div>
+                                        <div class="col-sm-9 vcenter"  >
+                                            <p>1 Hour, 15 Minutes</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-sm-3"  >
+                                            <h4><strong>Ingredients</strong></h4>
+                                        </div>
+                                        <div class="col-sm-9 vcenter"  >
+                                            <p>Peanuts, 1 lbs</p>
+                                            <p>Salt, 2 cups</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-sm-3"  >
+                                            <h4><strong>Cooking Instructions</strong></h4>
+                                        </div>
+                                        <div class="col-sm-9 vcenter"  >
+                                            <p>1. Combine peanuts and salt.</p>
+                                            <p>2. Mash everything together.</p>
+                                            <p>3. Enjoy!!</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                
+                            </div>
+								
+								
+						</form>
+						
+					</div>
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+	
+    <!-- jQuery -->
+    <script src="../js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../js/bootstrap.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="../js/plugins/morris/raphael.min.js"></script>
+    <script src="../js/plugins/morris/morris.min.js"></script>
+    <script src="../js/plugins/morris/morris-data.js"></script>
+
+</body>
+
+</html>
