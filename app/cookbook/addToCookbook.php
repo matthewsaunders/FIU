edd9891eaa4e210b1626801866e604fdedde4b49
romerodny <romerodny@gmail.com>
2015-04-21 16:55:42 -0400

    added search

--- app/cookbook/addToCookbook.php
+++ app/cookbook/addToCookbook.php
@@ -205,15 +205,15 @@ function displayForm(){
 			?>
 			</ul>
 			
-            <!-- Search Bar -->
-            <div class="col-sm-3 col-md-3 pull-right">
-                <form class="navbar-form" role="search">
+                      <!-- Search Bar -->
+                        <div class="col-sm-3 col-md-3 pull-right">
+        <form action="../search.php" method="POST" class="navbar-form" role="search">
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-            <div class="input-group-btn">
-                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                    <div class="input-group-btn">
+                        <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                    </div>
             </div>
-        </div>
         </form>
         </div>
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
