4b70ddd94d57c52dc527097bc1135b5093c1ba9e
msaun008 <msaun008@fiu.edu>
2015-04-21 14:31:40 -0400

    Fixed display errors in app/index.php, commented out search

--- app/index.php
+++ app/index.php
@@ -79,14 +79,14 @@ function displayDefaultPage(){
                 </li>
             </ul>
             
-            <form action="search.php" method="POST" class="navbar-form" role="search">
+            <!-- Search Bar -->
+            <div class="col-sm-3 col-md-3 pull-right">
+                <form class="navbar-form" role="search">
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-                    <div class="input-group-btn">
-                        <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                    </div>
+            <div class="input-group-btn">
+                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
             </div>
-        </form>
         </div>
         </form>
         </div>
