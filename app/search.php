5da848f8d2b6bf1b384d4bda34f50608c858792c
romerodny <romerodny@gmail.com>
2015-04-21 16:43:02 -0400

    Final commit

--- app/search.php
+++ app/search.php
@@ -219,7 +219,7 @@
                             <span class='caret'></span>
                             </button>
                             <ul class='dropdown-menu' role='menu' aria-labelledby='dropdownMenu1'>
-                                <li role='presentation'><a role='menuitem' tabindex='-1' href='#'>Add to my recipes</a></li>                        
+                                <li role='presentation'><a role='menuitem' tabindex='-1' href='/cookbook/addToCookBook.php'>Add to my recipes</a></li>                        
                             </ul>
                     </div>
 			     </div>
